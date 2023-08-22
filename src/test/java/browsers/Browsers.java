2cbe55c095ba0506736776679f4dbcd053f12fe9
gabriel-e2e <gabriel.sbrissa.nunes@gmail.com>
2023-08-22 12:17:15 -0300

    atualizacao2

--- src/test/java/browsers/Browsers.java
+++ src/test/java/browsers/Browsers.java
@@ -2,27 +2,21 @@ package browsers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class Browsers {
 	
 	public static WebDriver driver;
 	
 	public void abrirNavegador(String site) {
-		try{
-			
-			System.setProperty("webdriver.http.factory","jdk-http-client");
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--headless");
+		try {
+			System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 			driver = new ChromeDriver();
 			driver.get(site);
-			
 			driver.manage().window().maximize();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.err.println("---------- erro ao abrir navegador ---------- " + e.getMessage());
 			System.err.println("---------- causa do erro ----------" + e.getCause());
 		} 
-		
 	}
-
 }
